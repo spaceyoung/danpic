9beeeba6bd371ad5f46d61e397c0de6a82bccddd
minyoung <myoung.ever@gmail.com>
2024-09-08 00:59:24 +0900

    Design: create Header component and set layout
    - Header 컴포넌트 생성 후 레이아웃 설정

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <header>
+      <div className="header-inner">
+        <h1 className="logo">
+          <a href="#">
+            <figure>
+              <img src="" alt="단픽" />
+            </figure>
+            <strong>단픽</strong>
+          </a>
+        </h1>
+        <label className="toggle-switch">
+          <input />
+          <span />
+        </label>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
