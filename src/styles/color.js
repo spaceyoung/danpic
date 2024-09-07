ed98a6bdc0bdccf59962a6e6fe25db6ae946d32e
minyoung <154400292+spaceyoung@users.noreply.github.com>
2024-09-08 00:42:27 +0900

    [Feature/initial-setting] set project development environment (#2)
    * Chore: add libraries and set prettier rules
      - axios, emotion, react-icons, prettier, eslint-config-prettier 라이브러리 추가
      - prettier 규칙 설정
    
    * Chore: add styled-reset library
    
    * Design: add global style
      - 전역 스타일 설정 추가
    
    * Remove: delete unused files and codes
      - 사용하지 않는 App.css, index.css, react.svg, vite.svg 파일 및 코드 삭제
    
    * Design: add favicon file
    
    * Feat: set meta tags and change title
      - 웹 접근성을 위한 언어 설정 변경(en -> ko)
      - meta 태그 설정 추가
      - 사이트 title 변경
    
    * Design: add color system
      - 기준으로 사용할 컬러 시스템 추가

---
+++ src/styles/color.js
@@ -0,0 +1,25 @@
+const colorSystem = {
+  grayscale050: '#F1F3F3',
+  grayscale100: '#E4E6E7',
+  grayscale200: '#C8CDD0',
+  grayscale300: '#ADB4B8',
+  grayscale400: '#929BA0',
+  grayscale500: '#778288',
+  grayscale600: '#5F686D',
+  grayscale700: '#474E52',
+  grayscale800: '#2F3437',
+  grayscale900: '#181A1B',
+
+  primary050: '#EAF0FA',
+  primary100: '#D5E1F6',
+  primary200: '#ABC3ED',
+  primary300: '#81A5E4',
+  primary400: '#5787DB',
+  primary500: '#2D69D2',
+  primary600: '#2454A8',
+  primary700: '#1B3F7E',
+  primary800: '#122A54',
+  primary900: '#09152A',
+};
+
+export default colorSystem;
---
+++ src/styles/global.js
@@ -0,0 +1,83 @@
+import { css } from '@emotion/react';
+import reset from 'styled-reset';
+
+const globalStyle = css`
+  ${reset}
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+      format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Medium.woff')
+      format('woff');
+    font-weight: 500;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff')
+      format('woff');
+    font-weight: 600;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff')
+      format('woff');
+    font-weight: 700;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-ExtraBold.woff')
+      format('woff');
+    font-weight: 800;
+    font-style: normal;
+  }
+
+  :root {
+    font-synthesis: none;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-text-size-adjust: 100%;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  body {
+    min-width: 360px;
+    font-weight: 400;
+    line-height: 1.4;
+    font-family: Pretendard, Roboto, '돋움', dotum, AppleGothic, sans-serif;
+    letter-spacing: -0.5px;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: inherit;
+  }
+
+  img,
+  input,
+  select,
+  textarea {
+    display: block;
+  }
+
+  button {
+    all: unset;
+    cursor: pointer;
+  }
+`;
+
+export default globalStyle;
