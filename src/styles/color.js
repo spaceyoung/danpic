6d60f56b92813b4c8a6ea68a092b7a969848b63c
minyoung <myoung.ever@gmail.com>
2024-09-08 17:15:21 +0900

    Design: add theme color system
    - 라이드 모드, 다크 모드 테마 색상 시스템 추가

--- src/styles/color.js
+++ src/styles/color.js
@@ -26,4 +26,60 @@ const colorSystem = {
   shadow200: 'rgba(24, 26, 27, 0.2)',
 };
 
-export default colorSystem;
+// 라이트 모드
+export const lightThemeColor = {
+  text: {
+    logo: colorSystem.blue600,
+    title: colorSystem.grayscale800,
+    body: colorSystem.grayscale700,
+    detail: colorSystem.grayscale500,
+    label: colorSystem.grayscale400,
+    disabled: colorSystem.grayscale300,
+  },
+  emphasis: {
+    primary: colorSystem.blue600,
+    secondary: colorSystem.blue400,
+  },
+  background: {
+    default: colorSystem.grayscale000,
+    toggleSwitch: colorSystem.grayscale200,
+    banner: colorSystem.blue050,
+    article: colorSystem.grayscale000,
+  },
+  border: {
+    default: colorSystem.grayscale200,
+    banner: colorSystem.grayscale300,
+  },
+  shadow: {
+    default: colorSystem.shadow100,
+  },
+};
+
+// 다크 모드
+export const darkThemeColor = {
+  text: {
+    logo: colorSystem.grayscale100,
+    title: colorSystem.grayscale050,
+    body: colorSystem.grayscale100,
+    detail: colorSystem.grayscale200,
+    label: colorSystem.grayscale300,
+    disabled: colorSystem.grayscale400,
+  },
+  emphasis: {
+    primary: colorSystem.blue300,
+    secondary: colorSystem.blue200,
+  },
+  background: {
+    default: colorSystem.grayscale800,
+    toggleSwitch: colorSystem.blue300,
+    banner: colorSystem.blue600,
+    article: colorSystem.blue700,
+  },
+  border: {
+    default: colorSystem.grayscale500,
+    banner: colorSystem.grayscale400,
+  },
+  shadow: {
+    default: colorSystem.shadow200,
+  },
+};
