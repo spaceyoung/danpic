2cd0a73fecc544f9d64c1f11f0468cdb0625ca60
minyoung <myoung.ever@gmail.com>
2024-09-08 14:03:10 +0900

    Design: set Header component UI design
    - Header 컴포넌트의 전반적인 UI 디자인 설정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,104 @@
+import { css } from '@emotion/react';
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
+import colorSystem from '../../styles/color';
 import logo from '../../assets/logo.svg';
 
 function Header() {
   return (
-    <header>
-      <div className="header-inner">
-        <h1 className="logo">
-          <a href="#">
+    <header
+      css={css`
+        width: 100%;
+        height: 72px;
+        padding: 0 80px;
+        position: sticky;
+        left: 0;
+        top: 0;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+        `}
+      >
+        <h1>
+          <a
+            href="#"
+            css={css`
+              display: flex;
+              align-items: center;
+              gap: 8px;
+            `}
+          >
             <figure>
-              <img src={logo} alt="단픽" />
+              <img
+                src={logo}
+                alt="단픽"
+                css={css`
+                  width: 22px;
+                `}
+              />
             </figure>
-            <strong>단픽</strong>
+            <strong
+              css={css`
+                font-weight: 800;
+                font-size: 24px;
+                color: ${colorSystem.primary600};
+              `}
+            >
+              단픽
+            </strong>
           </a>
         </h1>
-        <label className="toggle-switch">
-          <input />
-          <span />
+        <label
+          css={css`
+            width: 52px;
+            height: 28px;
+            position: relative;
+          `}
+        >
+          <input
+            type="checkbox"
+            css={css`
+              width: 1px;
+              height: 1px;
+              clip-path: polygon(0 0, 0 0, 0 0);
+            `}
+          />
+          <span
+            css={css`
+              width: 100%;
+              height: 100%;
+              border-radius: 14px;
+              background-color: ${colorSystem.grayscale200};
+              position: absolute;
+              left: 0;
+              top: 0;
+              cursor: pointer;
+              ::before {
+                content: '';
+                width: 20px;
+                height: 20px;
+                border-radius: 50%;
+                background-color: white;
+                position: absolute;
+                left: 4px;
+                top: 4px;
+              }
+            `}
+          >
+            <MdLightMode
+              css={css`
+                font-size: 16px;
+                position: absolute;
+                left: 6px;
+                top: 6px;
+              `}
+            />
+          </span>
         </label>
       </div>
     </header>
