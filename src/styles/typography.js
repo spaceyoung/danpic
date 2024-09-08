aa85d4cd879dfc31735da715a02687961644616f
minyoung <myoung.ever@gmail.com>
2024-09-08 17:15:45 +0900

    Design: add typography style

---
+++ src/styles/typography.js
@@ -0,0 +1,50 @@
+const typography = {
+  // 기본 서체
+  body: {
+    fontFamily: "Pretendard, Roboto, '돋움', dotum, AppleGothic, sans-serif",
+    fontWeight: '400',
+    fontSize: '16px',
+    lineHeight: '1.4',
+    letterSpacing: '-0.5px',
+  },
+
+  // 로고
+  logo: {
+    fontWeight: '800',
+    fontSize: '24px',
+  },
+
+  // 섹션 제목
+  sectionTitle: {
+    fontWeight: '700',
+    fontSize: '32px',
+  },
+
+  // 배너
+  bannerTitle: {
+    fontWeight: '600',
+    fontSize: '22px',
+    lineHeight: '1.3',
+  },
+  bannerTranslation: {
+    fontWeight: '400',
+    fontSize: '18px',
+  },
+  bannerButton: {
+    fontWeight: '500',
+    fontSize: '18px',
+  },
+
+  // 기사
+  articleTitle: {
+    fontWeight: '600',
+    fontSize: '18px',
+    lineHeight: '1.3',
+  },
+  articleDate: {
+    fontWeight: '400',
+    fontSize: '14px',
+  },
+};
+
+export default typography;
