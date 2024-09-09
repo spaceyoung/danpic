61a1111f494d5b6bab02807bed1fcefdf515d7ed
minyoung <myoung.ever@gmail.com>
2024-09-09 17:59:08 +0900

    Design: set Translation component UI design

--- src/components/common/Translation/Translation.jsx
+++ src/components/common/Translation/Translation.jsx
@@ -1,22 +1,54 @@
+import { useTheme, css } from '@emotion/react';
+
 function Translation({
   isTranslated,
   isTranslateLoading,
   translateError,
   translationText,
 }) {
+  const theme = useTheme();
+
   if (isTranslateLoading)
     return (
       <>
-        <hr />
-        <p>번역 중...</p>
+        <hr
+          css={css`
+            width: 40%;
+            height: 1px;
+            margin: 20px 0 12px;
+            border: 0;
+            background-color: ${theme.color.border.banner};
+          `}
+        />
+        <p
+          css={css`
+            ${theme.typograhpy.bannerTranslation}
+          `}
+        >
+          번역 중...
+        </p>
       </>
     );
 
   if (translateError)
     return (
       <>
-        <hr />
-        <p>번역 중 오류가 발생했어요😢</p>
+        <hr
+          css={css`
+            width: 40%;
+            height: 1px;
+            margin: 20px 0 12px;
+            border: 0;
+            background-color: ${theme.color.border.banner};
+          `}
+        />
+        <p
+          css={css`
+            ${theme.typograhpy.bannerTranslation}
+          `}
+        >
+          번역 중 오류가 발생했어요😢
+        </p>
       </>
     );
 
@@ -24,8 +56,22 @@ function Translation({
     <>
       {isTranslated && (
         <>
-          <hr />
-          <p>{translationText}</p>
+          <hr
+            css={css`
+              width: 40%;
+              height: 1px;
+              margin: 20px 0 12px;
+              border: 0;
+              background-color: ${theme.color.border.banner};
+            `}
+          />
+          <p
+            css={css`
+              ${theme.typograhpy.bannerTranslation}
+            `}
+          >
+            {translationText}
+          </p>
         </>
       )}
     </>
