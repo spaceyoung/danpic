461e03f2e14fa3f477795da6a8d5ee46cc23800e
minyoung <myoung.ever@gmail.com>
2024-09-09 21:42:11 +0900

    Feat: realize article translate function using google API
    - 구글 번역 API를 이용하여 기사 번역 기능 구현

--- src/components/ArticleList/Article.jsx
+++ src/components/ArticleList/Article.jsx
@@ -1,10 +1,39 @@
+import { useState } from 'react';
+import axios from 'axios';
 import { useTheme, css } from '@emotion/react';
+import Translation from '../common/Translation/Translation';
 import TranslateButton from '../common/Button/TranslateButton';
 
 function Article({ publishDate, headline }) {
+  const [isTranslated, setIsTranslated] = useState(false);
+  const [isTranslateLoading, setIsTranslateLoading] = useState(false);
+  const [translateError, setTranslateError] = useState(null);
+  const [translationText, setTranslationText] = useState(null);
+
   const theme = useTheme();
 
   const formattedPublishDate = publishDate.substring(0, 10);
+
+  const translate = async () => {
+    if (!translationText) {
+      setIsTranslateLoading(true);
+      try {
+        const response = await axios.post(
+          `https://translation.googleapis.com/language/translate/v2?&key=${import.meta.env.VITE_GOOGLE_API_KEY}`,
+          { q: headline, target: 'ko' }
+        );
+        setTranslationText(response.data.data.translations[0].translatedText);
+        setIsTranslated(!isTranslated);
+      } catch (error) {
+        console.log(`기사 번역 중 오류 발생 | ${error}`);
+        setTranslateError(error);
+      }
+      setIsTranslateLoading(false);
+    } else {
+      // 이미 저장된 번역문이 있는 경우 API 재요청을 방지하고 토글 기능만 실행
+      setIsTranslated(!isTranslated);
+    }
+  };
 
   return (
     <article
@@ -37,6 +66,12 @@ function Article({ publishDate, headline }) {
         >
           {headline}
         </p>
+        <Translation
+          isTranslated={isTranslated}
+          isTranslateLoading={isTranslateLoading}
+          translateError={translateError}
+          translationText={translationText}
+        />
       </div>
       <div
         css={css`
@@ -44,7 +79,7 @@ function Article({ publishDate, headline }) {
           color: ${theme.color.emphasis.secondary};
         `}
       >
-        <TranslateButton />
+        <TranslateButton isTranslated={isTranslated} translate={translate} />
       </div>
     </article>
   );
