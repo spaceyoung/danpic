1e7253caf2c1137eab2ad7f0aa9aa853cd068a68
minyoung <myoung.ever@gmail.com>
2024-09-11 02:14:01 +0900

    Design: set ViewMoreButton component UI design

--- src/components/SectionTabs/ViewMoreButton.jsx
+++ src/components/SectionTabs/ViewMoreButton.jsx
@@ -1,5 +1,28 @@
+import { useTheme, css } from '@emotion/react';
+import { MdOutlineAddCircleOutline } from 'react-icons/md';
+
 function ViewMoreButton() {
-  return <button>더 보기</button>;
+  const theme = useTheme();
+
+  return (
+    <button
+      css={css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 4px;
+        width: 100%;
+        margin: 0 auto;
+        padding: 12px 20px;
+        border-radius: 8px;
+        font-weight: 500;
+        background-color: ${theme.color.background.viewMoreButton};
+        box-shadow: 3px 3px 10px 3px ${theme.color.shadow.default};
+      `}
+    >
+      <MdOutlineAddCircleOutline />더 보기
+    </button>
+  );
 }
 
 export default ViewMoreButton;
