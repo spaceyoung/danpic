31389188ad31f52c4e3ce8f0ea9e75d4c2a3117a
minyoung <myoung.ever@gmail.com>
2024-09-10 13:50:06 +0900

    Feat: create constants file to manage API
    - API 관련 값을 관리하기 위해 constants 폴더 및 파일 생성
    - API 요청 기본 주소(base url), 뉴욕 타임스 API 요청 URL 선언

---
+++ src/constants/api.js
@@ -0,0 +1,9 @@
+export const BASE_URL = {
+  NYT: 'https://api.nytimes.com/svc',
+  GOOGLE_TRANSLATE: 'https://translation.googleapis.com/language/translate/v2',
+};
+
+export const NYT_REQUEST_URL = {
+  TOP_STORIES: '/topstories/v2/home.json', // 배너 기사
+  SEARCH: '/search/v2/articlesearch.json', // 메인 콘텐츠 기사 목록
+};
