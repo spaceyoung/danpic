2c465cb75e392159716454e8641ec572b3bea1c5
minyoung <myoung.ever@gmail.com>
2024-09-12 08:58:49 +0900

    Design: change typography font size to responsive unit
    - typography 폰트 크기를 반응형 단위인 rem으로 변경

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -3,7 +3,7 @@ const typography = {
   body: {
     fontFamily: "Pretendard, Roboto, '돋움', dotum, AppleGothic, sans-serif",
     fontWeight: '400',
-    fontSize: '16px',
+    fontSize: '1rem',
     lineHeight: '1.4',
     letterSpacing: '-0.5px',
   },
@@ -11,39 +11,39 @@ const typography = {
   // 로고
   logo: {
     fontWeight: '800',
-    fontSize: '24px',
+    fontSize: '1.5rem',
   },
 
   // 섹션 제목
   sectionTitle: {
     fontWeight: '700',
-    fontSize: '32px',
+    fontSize: '2rem',
   },
 
   // 배너
   bannerSection: {
     fontWeight: '400',
-    fontSize: '15px',
+    fontSize: '0.95rem',
   },
   bannerTitle: {
     fontWeight: '600',
-    fontSize: '22px',
+    fontSize: '1.375rem',
     lineHeight: '1.3',
   },
   bannerButton: {
     fontWeight: '500',
-    fontSize: '16px',
+    fontSize: '1rem',
   },
 
   // 기사
   articleTitle: {
     fontWeight: '600',
-    fontSize: '18px',
+    fontSize: '1.125rem',
     lineHeight: '1.3',
   },
   articleDate: {
     fontWeight: '400',
-    fontSize: '14px',
+    fontSize: '0.875rem',
   },
 };
 
