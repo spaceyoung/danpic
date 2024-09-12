95c988c7404c7e3e98442a55fdb84b4c0b4d1b5d
minyoung <myoung.ever@gmail.com>
2024-09-12 10:35:49 +0900

    Design: restructure typography style setting
    - 타이포그래피 설정 재구성

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,4 +1,28 @@
 const typography = {
+  // 로고
+  logo: {
+    fontWeight: '800',
+    fontSize: '1.5rem',
+  },
+
+  // 섹션 헤더 제목
+  title1: {
+    fontWeight: '700',
+    fontSize: '2rem',
+  },
+  // 배너 기사 제목
+  title2: {
+    fontWeight: '600',
+    fontSize: '1.375rem',
+    lineHeight: '1.3',
+  },
+  // 메인 콘텐츠 기사 제목
+  title3: {
+    fontWeight: '600',
+    fontSize: '1.125rem',
+    lineHeight: '1.3',
+  },
+
   // 기본 서체
   body: {
     fontFamily: "Pretendard, Roboto, '돋움', dotum, AppleGothic, sans-serif",
@@ -8,48 +32,24 @@ const typography = {
     letterSpacing: '-0.5px',
   },
 
-  // 로고
-  logo: {
-    fontWeight: '800',
-    fontSize: '1.5rem',
-  },
-
-  // 섹션 제목
-  sectionTitle: {
-    fontWeight: '700',
-    fontSize: '2rem',
-  },
-
-  // 배너
-  bannerSection: {
-    fontWeight: '400',
+  // 배너 기사 섹션 이름
+  detail1: {
     fontSize: '0.95rem',
   },
-  bannerTitle: {
-    fontWeight: '600',
-    fontSize: '1.375rem',
-    lineHeight: '1.3',
-  },
-  bannerButton: {
-    fontWeight: '500',
-    fontSize: '1rem',
+  // 메인 콘텐츠 기사 발행일
+  detail2: {
+    fontSize: '0.875rem',
   },
 
-  // 탭 목록
-  tabList: {
+  // 탭 버튼
+  label1: {
     fontWeight: '500',
     fontSize: '1.25rem',
   },
-
-  // 기사
-  articleTitle: {
-    fontWeight: '600',
-    fontSize: '1.125rem',
-    lineHeight: '1.3',
-  },
-  articleDate: {
-    fontWeight: '400',
-    fontSize: '0.875rem',
+  // 배너, 더보기 버튼
+  label2: {
+    fontWeight: '500',
+    fontSize: '1rem',
   },
 };
 
