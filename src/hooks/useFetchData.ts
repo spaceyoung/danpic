71dd0016ce1bb18eeae2e5974c591b202ca60c57
minyoung <myoung.ever@gmail.com>
2024-10-03 05:20:25 +0900

    Refactor: separate create and input query parameter for readability
    - 가독성을 위해 쿼리 파라미터 생성 및 입력 과정 분리

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -11,24 +11,22 @@ const useFetchData = (
   const [fetchError, setFetchError] = useState<any>(null);
   const [fetchedData, setFetchedData] = useState<any>([]);
 
-  // section 값을 전달받았다면(섹션 탭 panel 기사) 해당하는 쿼리 파라미터를
-  // 그렇지 않으면(배너 기사) 빈 값을 생성 후
-  // .toString() 메서드를 통해 쿼리 파라미터를 문자열로 변환
-  const searchParams = new URLSearchParams(
-    section
-      ? {
-          fq: `section_name:("${section}")`,
-          page: `${pageNumber}`,
-          sort: 'newest',
-        }
-      : ''
-  ).toString();
+  const searchParams = {
+    fq: `section_name:("${section}")`,
+    page: `${pageNumber}`,
+    sort: 'newest',
+  };
+  const searchParamsString = new URLSearchParams(searchParams).toString();
 
   const fetchData = useCallback(async () => {
     setIsFetchLoading(true);
     setFetchError(null);
     try {
-      const response = await axiosNYT.get(`${requestURL}?${searchParams}`);
+      // section 값을 전달받았다면(섹션 탭 panel 기사) 생성한 searchParamsString 쿼리 파라미터를
+      // 그렇지 않으면(배너 기사) 빈 문자열을 입력
+      const response = await axiosNYT.get(
+        `${requestURL}?${section ? searchParamsString : ''}`
+      );
       setFetchedData(fetchedData.concat(response));
     } catch (error) {
       console.log(`기사 조회 중 오류 발생 | ${error}`);
